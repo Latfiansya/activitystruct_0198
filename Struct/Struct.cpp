5d4fb19459b0f9de037dc63ebda22071410f738a
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-02 10:44:48 +0700

    Merapikan barisan dan mengganti tipe data "Umur"

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -6,24 +6,24 @@ struct Mahasiswa
 	string nim;
 	string nama;
 	string alamat;
-	string umur;
+	int umur;
 };
 
 int main()
 {
 	Mahasiswa mhs;
-	cout << "Nomor Mahasiswa : ";
+	cout << "Nomor Mahasiswa\t: ";
 	cin >> mhs.nim;
-	cout << "Nama Mahasiswa : ";
+	cout << "Nama Mahasiswa\t: ";
 	cin >> mhs.nama;
-	cout << "Alamat Mahasiswa : ";
+	cout << "Alamat Mahasiswa: ";
 	cin >> mhs.alamat;
-	cout << "Umur Mahasiswa : ";
+	cout << "Umur Mahasiswa\t: ";
 	cin >> mhs.umur;
 
 	cout << endl;
-	cout << "\nNIM\t : " << mhs.nim;
-	cout << "\nNama\t : " << mhs.nama;
-	cout << "\nAlamat\t : " << mhs.alamat;
-	cout << "\nUmur\t : " << mhs.umur;
+	cout << "\nNIM\t: " << mhs.nim;
+	cout << "\nNama\t: " << mhs.nama;
+	cout << "\nAlamat\t: " << mhs.alamat;
+	cout << "\nUmur\t: " << mhs.umur;
 }
