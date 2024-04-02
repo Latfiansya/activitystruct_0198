7ac10b12bd091969d83f9448d04172bd22ce7158
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-02 12:15:11 +0700

    Menambahkan cin.ignore() untuk membersihkan karakter newline (\n) dari buffer

--- Struct_4/Struct_4.cpp
+++ Struct_4/Struct_4.cpp
@@ -1,23 +1,4 @@
-// Struct_4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future#include <iostream>
 #include <string>
 using namespace std;
 
@@ -40,6 +21,7 @@ int main()
 	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++)
 	{
+		cout << "Data ke-" << (i+1) << ":" << endl;
 		cout << "Nomor Mahasiswa\t: ";
 		getline(cin, mhs[i].nim);
 		cout << "Nama Mahasiswa\t: ";
@@ -50,10 +32,11 @@ int main()
 		cin >> mhs[i].alamat.desa;
 		cout << "\tNama Kota : ";
 		cin >> mhs[i].alamat.kota;
-
 		cout << "Umur Mahasiswa\t: ";
 		cin >> mhs[i].umur;
+		cin.ignore();
 	}
+
 	for (int i = 0; i < 3; i++)
 	{
 		cout << endl;
