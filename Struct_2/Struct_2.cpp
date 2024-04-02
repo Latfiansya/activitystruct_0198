c248072a937367c93cde14edd41ff21a45cfc109
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-02 11:03:48 +0700

    Menyesuaikan input data

--- Struct_2/Struct_2.cpp
+++ Struct_2/Struct_2.cpp
@@ -14,3 +14,27 @@ struct Mahasiswa
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa\t: ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa\t: ";
+	cin >> mhs.nama;
+
+	cout << "Alamat Mahasiswa\n";
+	cout << "\tNama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\tNama Kota : ";
+	cin >> mhs.alamat.kota;
+
+	cout << "Umur Mahasiswa\t: ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\nNIM : " << mhs.nim;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\nAlamat";
+	cout << "\nUmur : " << mhs.umur;
+}
