dbfed2f53d5aa5b0b267d9f0bf2346e522dd39cd
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-02 11:37:14 +0700

    Fix problem 1 : nama yang di-input tidak bisa lebih dari 1 kata
    Mengganti "cin" dengan "getline"

---
+++ Struct_3/Struct_3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa
+{
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main()
+{
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa\t: ";
+	getline(cin,mhs.nim);
+	cout << "Nama Mahasiswa\t: ";
+	getline(cin,mhs.nama);
+
+	cout << "Alamat Mahasiswa\n";
+	cout << "\tNama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\tNama Kota : ";
+	cin >> mhs.alamat.kota;
+
+	cout << "Umur Mahasiswa\t: ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\nNIM : " << mhs.nim;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\nAlamat";
+	cout << "\n\tDesa : " << mhs.alamat.desa;
+	cout << "\n\tKota : " << mhs.alamat.kota;
+	cout << "\nUmur : " << mhs.umur;
+}
